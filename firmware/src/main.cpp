8efff8444b76bfb5c1549b4b7763366e8bf96f8d
Derv6464 <dervlag6464@gmail.com>
2025-12-23 18:48:17 +0000

    setting up code -> i hate libraries

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,18 +1,32 @@
 #include <Arduino.h>
+#include <braiteoiri/include/IMU/IMU.h>
+#include <braiteoiri/include/IMU/MPU6050.h>
+#include <braiteoiri/include/Sensor.h>
+#include <comheadan/include/I2C/I2C_Handler.h>
+#include <comheadan/include/I2C/I2C_Pico.h>
 
-// put function declarations here:
-int myFunction(int, int);
+
+uint8_t addr = 0x68; // I2C address for MPU6050
+I2C_Handler* i2c = new I2C_Pico(addr);
+IMU* imu = new MPU6050(*i2c);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(115200);
+    Wire.begin();
+
+    platform = new PicoIMUPlatform(0x68);
+    imu = new MPU6050(*platform);
+    imu->init();
 }
 
+    
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+    imu_data imuData;
+    imu->read(imuData);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    Serial.print("Accel: ");
+    Serial.print("X: "); Serial.print(imuData.accel.x);
+    Serial.print(" Y: "); Serial.print(imuData.accel.y);
+    Serial.print(" Z: "); Serial.print(imuData.accel.z)
+    delay(100);
 }
