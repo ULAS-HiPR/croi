52dccc7731cd2a6cbad1b3faa5d7069468ab1f9c
Derv6464 <dervlag6464@gmail.com>
2026-02-13 20:06:34 +0000

    base compiling with static analysis

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,32 +1,118 @@
-#include <Arduino.h>
-#include <IMU/IMU.h>
-#include <IMU/MPU6050.h>
-#include <Sensor.h>
-#include <I2C/I2C_Handler.h>
-#include <I2C/I2C_Pico.h>
-#include <cstdint>
+#if F4
+#include "stm32f4xx_hal.h"
+#endif
+#include "cmsis_os.h"
 
-IMU* imu;
+#define LED_PIN                                GPIO_PIN_5
+#define LED_GPIO_PORT                          GPIOA
+#define LED_GPIO_CLK_ENABLE()                  __HAL_RCC_GPIOA_CLK_ENABLE()
 
+void SystemClock_Config(void);
+void Error_Handler(void);
 
-void setup() {
-    Serial.begin(115200);
-    Wire.begin();
-
-    uint8_t addr = 0x68; // I2C address for MPU6050
-    I2C_Handler* i2c = new I2C_Pico(addr);
-    IMU* imu = new MPU6050(*i2c);
-    imu->init();
+void StartBlickTask(void *argument)
+{
+  for (;;)
+  {
+    HAL_GPIO_TogglePin(LED_GPIO_PORT, LED_PIN);
+    osDelay(1000);
+  }
 }
 
-    
-void loop() {
-    imu_data imuData;
-    imu->update(&imuData);
+osThreadId_t blinkTaskHandle;
 
-    Serial.print("Accel: ");
-    Serial.print("X: "); Serial.print(imuData.accel.x);
-    Serial.print(" Y: "); Serial.print(imuData.accel.y);
-    Serial.print(" Z: "); Serial.print(imuData.accel.z);
-    delay(100);
+const osThreadAttr_t blinkTask_attributes = {
+    "blinkTask",          // name
+    0,                    // attr_bits
+    nullptr,              // cb_mem
+    0,                    // cb_size
+    nullptr,              // stack_mem
+    256 * 4,              // stack_size
+    osPriorityNormal,     // priority
+    0,                    // tz_module
+    0                     // reserved
+};
+
+int main(void)
+{
+  HAL_Init();
+  SystemClock_Config();
+  osKernelInitialize();
+
+ 
+  
+  LED_GPIO_CLK_ENABLE();
+  
+  GPIO_InitTypeDef GPIO_InitStruct;
+  
+  GPIO_InitStruct.Pin = LED_PIN;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(LED_GPIO_PORT, &GPIO_InitStruct); 
+
+  osKernelStart();
+  while (1)
+  {
+    HAL_GPIO_TogglePin(LED_GPIO_PORT, LED_PIN);
+    
+    HAL_Delay(1000);
+  }
+}
+
+
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 16;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+void Error_Handler(void)
+{
+  __disable_irq();
+  while (1)
+  {
+    // stay here
+  }
 }
